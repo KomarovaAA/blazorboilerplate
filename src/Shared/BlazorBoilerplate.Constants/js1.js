cbd7b666934299017807f1698bbd5d6951c8bd9d
Alena Komarova <Alena.Komarova@jetbrains.com>
2021-12-10 18:31:47 +0300

    v

---
+++ src/Shared/BlazorBoilerplate.Constants/js1.js
@@ -0,0 +1,18 @@
+﻿result = 1 + 1
+
+var myLink = {
+        img: "btn.gif" ,
+        text: "Button" ,
+        width: 128
+    } ,
+    local = true ,
+    initial = -1;
+cssClasses = ["bold" , "red"]
+selector = "#id";
+
+color = "red";
+offset = 10;
+
+varName = val;
+
+console.log(result)
